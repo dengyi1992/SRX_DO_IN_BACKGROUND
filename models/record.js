c7adad6b97e40eb2c8795251a81ea4e20b471c8a
978548481@qq.com <d7577355>
2016-05-12 16:38:37 +0800

    添加数据清除功能并插入数据库中

--- models/record.js
+++ models/record.js
@@ -14,19 +14,19 @@ module.exports=Record;
 Record.prototype.save=function (callback) {
     var date = new Date();
     //存储各种时间格式，方便以后扩展
-    var time = {
-        date: date,
-        year: date.getFullYear(),
-        month: date.getFullYear() + "-" + (date.getMonth() + 1),
-        day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
-        minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
-        date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
-    };
+    // var time = {
+    //     date: date,
+    //     year: date.getFullYear(),
+    //     month: date.getFullYear() + "-" + (date.getMonth() + 1),
+    //     day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+    //     minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+    //     date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+    // };
     var record={
         operator:this.operator,
         operatortype:this.operatortype,
         operate:this.operate,
-        time:time
+        time:date
     };
     mongodb.open(function (err, db) {
         if (err){
